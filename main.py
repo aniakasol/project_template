35a32494ebc943e704abe4d2791359ebee6aab09
Anna Krasno <annakrasn2212@gmail.com>
2026-03-12 00:38:23 +0200

    io function initial tests added to main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from app.io import input 
+from app.io import output
+
 def main():
-    pass
+    try:
+        input1 = input.input_from_console()
+        write(input1)
+        input2 = input.read_file("data/data_for_input.txt")
+        write(input2)
+        input3 = input.read_file_with_pandas("data/data_for_input.txt")
+        write(input3)
+    except Exception as e:
+        print(e)
+
+def write(input:str):
+    output.output_to_console(input)
+    output.write_to_file("data/data_output.txt", input)
 
 if __name__ == "__main__":
     main()
